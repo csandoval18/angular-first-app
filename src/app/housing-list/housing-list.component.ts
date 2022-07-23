066a647247addf0e403d5244aaed15b2cdb21b01
Christian AS <alvitesscf18@uww.edu>
2022-07-23 01:45:25 -0500

    update

--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HousingLocation } from '../housing-location';
 
 @Component({
@@ -7,11 +7,26 @@ import { HousingLocation } from '../housing-location';
   styleUrls: ['./housing-list.component.scss'],
 })
 export class HousingListComponent implements OnInit {
+  // Declaring property locationList
+  // Input decorator is used to pass data from parent element to children
+
+  @Input() locationList: HousingLocation[] = [];
+  results: HousingLocation[] = [];
+
+  @Output() selectedLocationEvent = new EventEmitter<HousingLocation>();
+
   constructor() {}
 
   ngOnInit(): void {}
 
   searchHousingLocations(searchValue: string) {
-    console.log(searchValue);
+    if (!searchValue) return;
+    this.results = this.locationList.filter((location) =>
+      location.city.toLowerCase().includes(searchValue.toLowerCase())
+    );
+  }
+
+  selectHousingLocation(location: HousingLocation) {
+    this.selectedLocationEvent.emit(location);
   }
 }
