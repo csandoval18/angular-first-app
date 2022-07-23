3a1655db5c455c2dc5f71e0fc4d0404eec9c1bf0
Christian AS <alvitesscf18@uww.edu>
2022-07-23 00:51:24 -0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HousingLocation } from './housing-location';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'fairhouse';
--- src/app/housing-list/housing-list.component.ts
+++ src/app/housing-list/housing-list.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { HousingLocation } from '../housing-location';
 
 @Component({
   selector: 'app-housing-list',
   templateUrl: './housing-list.component.html',
-  styleUrls: ['./housing-list.component.sass']
+  styleUrls: ['./housing-list.component.scss'],
 })
 export class HousingListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  searchHousingLocations(searchValue: string) {
+    console.log(searchValue);
   }
-
 }
